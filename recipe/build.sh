fcdd78408f8a16533945ef70f5cde9e6eca78631
Mervin Fansler <mef3005@med.cornell.edu>
2023-03-26 11:21:30 -0400

    log1p

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## see https://github.com/conda-forge/r-base-feedstock/issues/163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
